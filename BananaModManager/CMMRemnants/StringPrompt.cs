d894e2c67bb5fb947878849e0bab8ad48f234904
Bingus <bendawsdev@gmail.com>
2025-04-04 13:01:04 -0500

    import the old string prompt lib i made for CMM

---
+++ BananaModManager/CMMRemnants/StringPrompt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BananaModManager.CMMRemnants
+{
+    internal class StringPrompt
+    {
+        public static string Prompt(string message)
+        {
+            StringPromptBox stringPromptBox = new StringPromptBox(message);
+            stringPromptBox.ShowDialog();
+
+            while (stringPromptBox.Output == "")
+            {
+                Task.Delay(100);
+            }
+
+            return stringPromptBox.Output;
+        }
+    }
+}
---
+++ BananaModManager/CMMRemnants/StringPromptBox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BananaModManager.CMMRemnants
+{
+    public partial class StringPromptBox : Form
+    {
+        public string Output = "";
+        public StringPromptBox(string message)
+        {
+            InitializeComponent();
+            this.label.Text = message;
+
+            okButton.Click += (sender, e) =>
+            {
+                Output = text.Text;
+                this.Close();
+            };
+
+            cancelButton.Click += (sender, e) =>
+            {
+                Output = null;
+                this.Close();
+            };
+        }
+
+        private void text_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
